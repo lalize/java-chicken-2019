fe57c17cb566e010f45427b4e336a8153b490b0d
lalize <printf@kakao.com>
2020-01-10 17:07:15 +0900

    feat: 테이블을 선택하는 기능 추가

--- src/main/java/domain/OrderFunction.java
+++ src/main/java/domain/OrderFunction.java
@@ -1,8 +1,20 @@
 package domain;
 
+import java.util.List;
+
+import view.InputView;
+import view.OutputView;
+
 public class OrderFunction implements Function {
 	@Override
 	public void call() {
+		final Table table = selectTable();
+	}
 
+	private Table selectTable() {
+		final List<Table> tables = TableRepository.tables();
+		OutputView.printTables(tables);
+		final int tableNumber = InputView.inputTableNumber();
+		return TableRepository.valueOf(tableNumber);
 	}
 }
